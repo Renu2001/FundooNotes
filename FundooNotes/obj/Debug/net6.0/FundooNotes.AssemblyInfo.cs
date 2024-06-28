70a89211782a6d7b1a26839fdec1646a581e73d4
Renuka <renu.bagave1901@gmail.com>
2024-06-28 16:49:35 +0530

    [Renuka]Added : Added Consumer App Project from testing Rabbit mq

--- FundooNotes/obj/Debug/net6.0/FundooNotes.AssemblyInfo.cs
+++ FundooNotes/obj/Debug/net6.0/FundooNotes.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FundooNotes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+305967865bbdcd24222cb4bcab1c6f5c367c6b70")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("FundooNotes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FundooNotes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
